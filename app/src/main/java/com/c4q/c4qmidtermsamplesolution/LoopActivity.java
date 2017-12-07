cb72ce4afcb682e4d1281f7b27f7ec182fd82172
AndrewOrobator <Orobator.Andrew@gmail.com>
2017-12-07 18:39:22 -0500

    Initial commit

---
+++ app/src/main/java/com/c4q/c4qmidtermsamplesolution/LoopActivity.java
@@ -0,0 +1,12 @@
+package com.c4q.c4qmidtermsamplesolution;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoopActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_loop);
+  }
+}
